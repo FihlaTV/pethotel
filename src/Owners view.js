c988f8072b25416e15ce98f286f656ff360f3f49
vv <vv@altsoft.biz>
2013-08-13 17:41:46 +0400

    Pets info for owners grid added

--- src/Owners view.js
+++ src/Owners view.js
@@ -39,7 +39,6 @@ function grdOwnersMouseClicked(evt) {//GEN-FIRST:event_grdOwnersMouseClicked
  * @param evt Event object
  */
 function btnSearchActionPerformed(evt) {//GEN-FIRST:event_btnSearchActionPerformed
-    petsUptates = [];
     lastNamePattern = '%' + txtSearch.text + '%';
 }//GEN-LAST:event_btnSearchActionPerformed
 
@@ -53,21 +52,35 @@ function btnReportActionPerformed(evt) {//GEN-FIRST:event_btnReportActionPerform
     ownersReport.show();
 }//GEN-LAST:event_btnReportActionPerformed
 
+/**
+ * Search text control event handler.
+ * @param evt Event object
+ */
 function txtSearchActionPerformed(evt) {//GEN-FIRST:event_txtSearchActionPerformed
     btnSearchActionPerformed(null);
 }//GEN-LAST:event_txtSearchActionPerformed
 
+/**
+ * Pet's column onRender handler.
+ * @param evt onRender event object
+ * @returns true to apply changes to the cell
+ */
 function petsOnRender(evt) {//GEN-FIRST:event_petsOnRender
-    var txt = "";
+    var txt = '';
     ownersPets.find(ownersPets.md.OWNERS_ID, evt.id).forEach(function(aPet) {
-        if(txt.length > 0)
+        if(txt.length > 0) {
             txt += ' ';
+        }
         txt += aPet.NAME ? aPet.NAME : '';
     });
     evt.cell.display = txt;
     return true;
 }//GEN-LAST:event_petsOnRender
 
+/**
+ * Called then data is ready in ownersPets entity.
+ * @param evt Event object
+ */
 function ownersPetsOnRequeried(evt) {//GEN-FIRST:event_ownersPetsOnRequeried
     owners.params.lastNamePattern = ownersPets.params.lastNamePattern;
     owners.requery();
